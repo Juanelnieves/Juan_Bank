0bf878729cf6cd0cf6d8bef67d2b0ccd697ae49f
Your Name <you@example.com>
2023-12-07 21:50:45 +0100

    credit card arreglo

--- view/dashboard.php
+++ view/dashboard.php
@@ -11,6 +11,8 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
   <!-- Custom CSS -->
   <link href="../styles/css/styles.css" rel="stylesheet">
+  <link href="../styles/css/credit_card.css" rel="stylesheet">
+
 </head>
 
 <body>
@@ -75,14 +77,14 @@
       </div>
 
 
-      <section id="card" class="card">
+      <section id="credit_card" class="credit_card">
         <div id="highlight"></div>
-        <section class="card__front">
-          <div class="card__header">
+        <section class="credit_card__front">
+          <div class="credit_card__header">
             <div>CreditCard</div>
             <svg xmlns="http://www.w3.org/2000/svg" height="40" width="60" id="svg895" version="1.1" viewBox="-96 -98.908 832 593.448"><defs id="defs879"><style id="style877" type="text/css">.e{fill:#f79e1b}</style></defs><path id="rect887" display="inline" fill="#ff5f00" stroke-width="5.494" d="M224.833 42.298h190.416v311.005H224.833z"/><path id="path889" d="M244.446 197.828a197.448 197.448 0 0175.54-155.475 197.777 197.777 0 100 311.004 197.448 197.448 0 01-75.54-155.53z" fill="#eb001b" stroke-width="5.494"/><path id="path891" d="M621.101 320.394v-6.372h2.747v-1.319h-6.537v1.319h2.582v6.373zm12.691 0v-7.69h-1.978l-2.307 5.493-2.308-5.494h-1.977v7.691h1.428v-5.823l2.143 5h1.483l2.143-5v5.823z" class="e" fill="#f79e1b" stroke-width="5.494"/><path id="path893" d="M640 197.828a197.777 197.777 0 01-320.015 155.474 197.777 197.777 0 000-311.004A197.777 197.777 0 01640 197.773z" class="e" fill="#f79e1b" stroke-width="5.494"/></svg>
           </div>
-          <div id="card_number" class="card__number">
+          <div id="credit_card_number" class="credit_card__number">
             <span>#<br></span>
             <span>#<br></span>
             <span>#<br></span>
@@ -103,77 +105,28 @@
             <span>#<br></span>
             <span>#<br></span>
           </div>
-          <div class="card__footer">
-            <div class="card__holder">
-              <div class="card__section__title">Card Holder</div>
-              <div id="card_holder">Name on card</div>
+          <div class="credit_card__footer">
+            <div class="credit_card__holder">
+              <div class="credit_card__section__title">Card Holder</div>
+              <div id="credit_card_holder">Name on card</div>
             </div>
-            <div class="card__expires">
-              <div class="card__section__title">Expires</div>
-              <span id="card_expires_month">MM</span>/<span id="card_expires_year">YY</span>
+            <div class="credit_card__expires">
+              <div class="credit_card__section__title">Expires</div>
+              <span id="credit_card_expires_month">MM</span>/<span id="credit_card_expires_year">YY</span>
             </div>
           </div>
         </section>
-        <section class="card__back">
-          <div class="card__hide_line"></div>
+        <section class="credit_card__back">
+          <div class="credit_card__hide_line"></div>
 
-          <div class="card_cvv">
+          <div class="credit_card_cvv">
             <span>CVV</span>
-            <div id="card_cvv_field" class="card_cvv_field"></div>
+            <div id="credit_card_cvv_field" class="credit_card_cvv_field"></div>
           </div>
         </section>
       </section>
 
-      <form class="form">
-        <div>
-          <label for="number">Card Number</label>
-          <input id="number" type="number">
-        </div>
-        <div>
-          <label for="holder">Card Holder</label>
-          <input id="holder" type="text">
-        </div>
-        <div class="filed__group">
-          <div>
-            <label for="expiration_month">Expiration Date</label>
-            <div class="filed__date">
-              <select id="expiration_month">
-                <option selected disabled>Month</option>
-                <option>01</option>
-                <option>02</option>
-                <option>03</option>
-                <option>04</option>
-                <option>05</option>
-                <option>06</option>
-                <option>07</option>
-                <option>08</option>
-                <option>09</option>
-                <option>10</option>
-                <option>11</option>
-                <option>12</option>
-              </select>
-              <select id="expiration_year">
-                <option selected disabled>Year</option>
-                <option>2023</option>
-                <option>2024</option>
-                <option>2025</option>
-                <option>2026</option>
-                <option>2027</option>
-                <option>2028</option>
-                <option>2029</option>
-                <option>2030</option>
-                <option>2031</option>
-                <option>2032</option>
-              </select>
-            </div>
-          </div>
-          <div>
-            <label for="cvv">CVV</label>
-            <input id="cvv" type="number">
-          </div>
-        </div>
-      </form>
-
+      
       <br>
 
       <div class="row">
