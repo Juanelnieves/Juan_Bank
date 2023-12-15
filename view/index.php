d36ed9b54574dc9bf2857380bca49e87298a991e
Juan <juangonzalezfernandez004@alumnos.ilernasevilla.com>
2023-12-15 14:14:55 +0100

    FIN DE PROYECTO-PAGINA DE INICIO

---
+++ view/index.php
@@ -0,0 +1,62 @@
+<html lang="en">
+
+<head>
+    <title>Title</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../styles/css/home-styles.css" rel="stylesheet">
+
+</head>
+
+<body>
+    <!-- Barra navegación -->
+    <div class="navbar">
+        <div class="navbar-brand">NEO</div>
+        <div>
+            <a href="index.php" class="nav-item signup">Home</a>
+            <a href="register.php" class="nav-item signup">Sign Up</a>
+            <a href="login.php" class="nav-item login">Login</a>
+        </div>
+    </div>
+
+    <!-- Banner -->
+    <div class="bank-banner">
+        <img src="../img/LOGO.PNG" alt="Bank Logo" class="bank-logo">
+        <div class="banner-content">
+            <h1>Welcome to NEO Bank</h1>
+            <p>Secure. Reliable. Your financial partner.</p>
+        </div>
+    </div>
+
+    <!-- Video -->
+    <div class="video-section">
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/ndTbiDQjbiE?si=LX56HPmdg5wPGqxA" frameborder="0" allowfullscreen></iframe>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/GPIaPC4Nfkc?si=c51fQIyubPqzfyTR" frameborder="0" allowfullscreen></iframe>
+    </div>
+
+    <BR>
+
+    <!-- Footer -->
+    <footer class="footer">
+        <div class="footer-logo-brand">
+            <div class="footer-logo">
+                <img src="../img/LOGO.PNG" alt="NEO LOGO">
+            </div>
+            <div class="footer-brand">NEO</div>
+        </div>
+        <div class="footer-contact">
+            <h6>CONTACT US</h6>
+            <p>Bahnhofstrasse 9, 8001 Zürich, Suiza</p>
+            <p>contac@neo.com</p>
+            <p>+41 844 843 823</p>
+        </div>
+    </footer>
+
+
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js" integrity="sha384-BBtl+eGJRgqQAUMxJ7pMwbEyER4l1g+O15P+16Ep7Q9Q+zqX6gSbd85u4mG4QzX+" crossorigin="anonymous"></script>
+</body>
+
+</html>
